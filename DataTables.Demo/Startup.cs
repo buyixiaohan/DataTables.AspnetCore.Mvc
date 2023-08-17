9112510900080007b612975002dd06e656d58b22
Honor <wang97322@foxmail.com>
2023-08-17 20:38:50 +0800

    全面需改为net6

--- DataTables.Demo/Startup.cs
+++ DataTables.Demo/Startup.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json.Linq;
 
 namespace DataTables.Demo
 {
@@ -31,7 +26,7 @@ namespace DataTables.Demo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                //app.UseBrowserLink();
             }
             else
             {
